b123821a1df751e190d3fb3e043c6d0d61f4767c
surya <suryakondisetti741@gmail.com>
2022-09-16 11:43:51 +0100

    This is my FoodBox

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -109,6 +109,103 @@ return[
   origins:['vijaywada','Rajahmundry','Chennai'],
   Time:'60-110'
 
+},
+
+{
+  id:9,
+  price:30,
+  name:'idli',
+  notfavourite:true,
+  star:3.0,
+  tags:['Dinner','Lunch','Breakfast'],
+  image:'assets/images/pexels-cats-coming-674574.jpg',
+  origins:['vijaywada','Rajahmundry','Banglore'],
+  Time:'70-120'
+
+},
+{
+  id:10,
+  price:80,
+  name:'Cake',
+  notfavourite:false,
+  star:3.0,
+  tags:['Breakfast'],
+  image:'assets/images/pexels-jess-bailey-designs-913136.jpg',
+  origins:['vijaywada','Rajahmundry','hyderabad'],
+  Time:'40-80'
+
+},
+{
+  id:11,
+  price:150,
+  name:'Roast',
+  notfavourite:false,
+  star:4.0,
+  tags:['Dinner','Lunch','Breakfast'],
+  image:'assets/images/pexels-chan-walrus-1059943.jpg',
+  origins:['vijaywada','Chennai'],
+  Time:'60-150'
+
+},
+{
+  id:12,
+  price:110,
+  name:'Burger',
+  notfavourite:false,
+  star:3.0,
+  tags:['Dinner','Lunch','Breakfast'],
+  image:'assets/images/pexels-rajesh-tp-1633578.jpg',
+  origins:['vijaywada','Rajahmundry','Chennai'],
+  Time:'60-110'
+
+},
+{
+  id:13,
+  price:180,
+  name:'Secretes',
+  notfavourite:false,
+  star:4.0,
+  tags:['Dinner','Lunch','Breakfast'],
+  image:'assets/images/pexels-saveurs-secretes-5410407.jpg',
+  origins:['vijaywada','Banglore','vizag'],
+  Time:'60-80'
+
+},
+{
+  id:14,
+  price:50,
+  name:'Sandra',
+  notfavourite:false,
+  star:3.0,
+  tags:['Dinner','Lunch','Breakfast'],
+  image:'assets/images/pexels-sandra-filipe-6183633.jpg',
+  origins:['vijaywada','Rajahmundry','Chennai'],
+  Time:'60-110'
+
+},
+{
+  id:15,
+  price:40,
+  name:'Chocolate',
+  notfavourite:false,
+  star:3.0,
+  tags:['Breakfast'],
+  image:'assets/images/pexels-pixabay-45202.jpg',
+  origins:['vijaywada','Chennai'],
+  Time:'60-110'
+
+},
+{
+  id:16,
+  price:120,
+  name:'Trangdoan',
+  notfavourite:false,
+  star:3.0,
+  tags:['Breakfast'],
+  image:'assets/images/pexels-trang-doan-1092730.jpg',
+  origins:['vijaywada','Chennai','Banglore'],
+  Time:'60-110'
+
 }
   
 ]
